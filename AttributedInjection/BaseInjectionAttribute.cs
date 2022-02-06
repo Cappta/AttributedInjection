ae01ed2f7ccce36dae2f85968621fa98a0ca55d4
SammyROCK <samfmf@gmail.com>
2022-02-06 18:58:12 -0300

    Updated project to OpenSource

--- AttributedInjection/BaseInjectionAttribute.cs
+++ AttributedInjection/BaseInjectionAttribute.cs
@@ -1,11 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace AttributedInjection;
+namespace AttributedInjection {
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public abstract class BaseInjectionAttribute : Attribute {
+		public Type? ServiceType { get; set; }
 
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public abstract class BaseInjectionAttribute : Attribute {
-	public Type? ServiceType { get; set; }
-
-	public abstract ServiceLifetime ServiceLifetime { get; }
+		public abstract ServiceLifetime ServiceLifetime { get; }
+	}
 }
--- AttributedInjection/IServiceCollectionExtensions.cs
+++ AttributedInjection/IServiceCollectionExtensions.cs
@@ -3,66 +3,66 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 
-namespace AttributedInjection;
-
-public static class IServiceCollectionExtensions {
-	private static IServiceCollection IdempotentAdd(this IServiceCollection services, ServiceDescriptor serviceDescriptor) {
-		if(services.Any(sd =>
-			sd.ServiceType == serviceDescriptor.ServiceType &&
-			sd.ImplementationInstance == serviceDescriptor.ImplementationInstance &&
-			sd.ImplementationType == serviceDescriptor.ImplementationType &&
-			sd.ImplementationFactory == serviceDescriptor.ImplementationFactory
-			) == false
-		) {
-			services.Add(serviceDescriptor);
-		}
-		return services;
-	}
-
-	public static IServiceCollection AddAttributedInjections(this IServiceCollection services) {
-		foreach(var (type, injectAttributes) in AppDomain.CurrentDomain.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
-			foreach(var injectAttribute in injectAttributes) {
-				var serviceType = injectAttribute.ServiceType;
-				if(serviceType is null) {
-					var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
-					if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
-					serviceType = assumedType;
-				}
-				services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
+namespace AttributedInjection {
+	public static class IServiceCollectionExtensions {
+		private static IServiceCollection IdempotentAdd(this IServiceCollection services, ServiceDescriptor serviceDescriptor) {
+			if(services.Any(sd =>
+				sd.ServiceType == serviceDescriptor.ServiceType &&
+				sd.ImplementationInstance == serviceDescriptor.ImplementationInstance &&
+				sd.ImplementationType == serviceDescriptor.ImplementationType &&
+				sd.ImplementationFactory == serviceDescriptor.ImplementationFactory
+				) == false
+			) {
+				services.Add(serviceDescriptor);
 			}
+			return services;
 		}
-		return services;
-	}
 
-	public static IServiceCollection AddAttributedInjectionsFromAssemblyContaining<T>(this IServiceCollection services) {
-		foreach(var (type, injectAttributes) in typeof(T).Assembly.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
-			foreach(var injectAttribute in injectAttributes) {
-				var serviceType = injectAttribute.ServiceType;
-				if(serviceType is null) {
-					var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
-					if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
-					serviceType = assumedType;
+		public static IServiceCollection AddAttributedInjections(this IServiceCollection services) {
+			foreach(var (type, injectAttributes) in AppDomain.CurrentDomain.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
+				foreach(var injectAttribute in injectAttributes) {
+					var serviceType = injectAttribute.ServiceType;
+					if(serviceType is null) {
+						var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
+						if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
+						serviceType = assumedType;
+					}
+					services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
 				}
-				services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
 			}
+			return services;
 		}
-		return services;
-	}
 
-	public static IServiceCollection AddAttributedInjectionsFromNamespaceOf<T>(this IServiceCollection services) {
-		foreach(var (type, injectAttributes) in typeof(T).Assembly.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
-			if(type.Namespace != typeof(T).Namespace) { continue; }
-
-			foreach(var injectAttribute in injectAttributes) {
-				var serviceType = injectAttribute.ServiceType;
-				if(serviceType is null) {
-					var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
-					if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
-					serviceType = assumedType;
+		public static IServiceCollection AddAttributedInjectionsFromAssemblyContaining<T>(this IServiceCollection services) {
+			foreach(var (type, injectAttributes) in typeof(T).Assembly.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
+				foreach(var injectAttribute in injectAttributes) {
+					var serviceType = injectAttribute.ServiceType;
+					if(serviceType is null) {
+						var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
+						if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
+						serviceType = assumedType;
+					}
+					services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
 				}
-				services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
 			}
+			return services;
 		}
-		return services;
+
+		public static IServiceCollection AddAttributedInjectionsFromNamespaceOf<T>(this IServiceCollection services) {
+			foreach(var (type, injectAttributes) in typeof(T).Assembly.EnumerateTypesWithAttributes<BaseInjectionAttribute>()) {
+				if(type.Namespace != typeof(T).Namespace) { continue; }
+
+				foreach(var injectAttribute in injectAttributes) {
+					var serviceType = injectAttribute.ServiceType;
+					if(serviceType is null) {
+						var assumedType = type.GetInterfaces().FirstOrDefault() ?? type;
+						if(assumedType.IsGenericType) { assumedType = assumedType.GetGenericTypeDefinition(); }
+						serviceType = assumedType;
+					}
+					services.IdempotentAdd(new ServiceDescriptor(serviceType, type, injectAttribute.ServiceLifetime));
+				}
+			}
+			return services;
+		}
 	}
 }
